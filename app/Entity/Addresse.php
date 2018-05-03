c9f0c72b3d2d989e8f8f7bf6554dffbdc3e2e121
Rackham <ugo@rougelot.fr>
2018-05-03 14:50:50 +0200

    ajout des classes dans Entity

---
+++ app/Entity/Addresse.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ugo
+ * Date: 03/05/2018
+ * Time: 14:46
+ */
+
+class Address
+{
+    private $numero;
+    private $rue;
+    private $codePostale;
+    private $ville;
+
+    public function __construct($numero, $rue, $codePostal, $ville)
+    {
+        $this->numero = $numero;
+        $this->rue = $rue;
+        $this->codePostale = $codePostal;
+        $this->ville = $ville;
+    }
+}
---
+++ app/Entity/Personne.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ugo
+ * Date: 03/05/2018
+ * Time: 14:49
+ */
+
+class Personne
+{
+    private $addresse;
+}
